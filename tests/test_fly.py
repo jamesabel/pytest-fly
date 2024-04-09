df17ab55adb9a93e9875aa1e34f240a431832575
JamesAbel <j@abel.co>
2024-04-09 14:55:37 -0700

    initial dev

--- tests/test_fly.py
+++ tests/test_fly.py
@@ -3,16 +3,16 @@ import time
 from src.pytest_fly.db import get_most_recent_start_and_finish
 
 
+def test_simple_0():
+    test_name, start, finish = get_most_recent_start_and_finish()
+    print(f"{test_name=}, {start=}, {finish=}")
+
+
 def test_simple_1():
-    time.sleep(3)
+    time.sleep(1)
     print("1")
 
 
 def test_simple_2():
-    time.sleep(6)
+    time.sleep(2)
     print("2")
-
-
-def test_start_finish():
-    test_name, start, finish = get_most_recent_start_and_finish()
-    print(f"{test_name=}, {start=}, {finish=}")

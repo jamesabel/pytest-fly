2cb412a63ea0a0145b51d196874b694e7853cdc2
JamesAbel <j@abel.co>
2025-02-23 22:42:07 -0800

    dev continues

--- src/pytest_fly/app/view/about.py
+++ src/pytest_fly/app/view/about.py
@@ -1,7 +1,11 @@
+from dataclasses import asdict
+
+import humanize
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout
 
 from .gui_util import PlainTextWidget
 from ..project_info import get_project_info
+from ..platform_info import get_platform_info
 
 
 class About(QWidget):
@@ -10,10 +14,19 @@ class About(QWidget):
 
         self.setWindowTitle("About")
 
-        project_info = get_project_info()
+        text_lines = []
+        for key, value in asdict(get_project_info()).items():
+            text_lines.append(f"{key}: {value}")
+        text_lines.append("")
+
+        for key, value in get_platform_info().items():
+            if any([descriptor in key.lower() for descriptor in ["cache", "memory"]]):
+                text_lines.append(f"{key}: {humanize.naturalsize(value)}")
+            else:
+                text_lines.append(f"{key}: {value}")
 
         about_box = PlainTextWidget()
-        about_box.set_text(str(project_info))
+        about_box.set_text("\n".join(text_lines))
 
         horizontal_layout = QHBoxLayout()
         horizontal_layout.addWidget(about_box)

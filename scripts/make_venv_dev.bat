04c9c4b95b7ebef4b9f19b0bb4650b4f676a5658
JamesAbel <j@abel.co>
2025-11-30 11:19:49 -0800

    Remove pinned requirements file

--- scripts/make_venv_dev.bat
+++ scripts/make_venv_dev.bat
@@ -5,6 +5,4 @@ c:"\Program Files\Python314\python.exe" -m venv --clear .venv
 .venv\Scripts\python.exe -m pip install --upgrade pip
 .venv\Scripts\pip3 install -U setuptools
 .venv\Scripts\pip3 install -U -r requirements-dev.txt
-del requirements.txt
-.venv\Scripts\python.exe -m piptools compile -q --strip-extras --output-file requirements.txt requirements-dev.txt
 popd
